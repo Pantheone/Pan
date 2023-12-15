8b33ffdc06fd47c0302aa584ead3b4a1f7a0b6b3
Pantulip <terpelov_p@weberlibs.ru>
2023-12-15 13:10:01 +0300

    15.12.23

--- mark_a_pass.py
+++ mark_a_pass.py
@@ -1,11 +1,10 @@
 import time
-import telebot
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
 
 option = webdriver.ChromeOptions()
+
 driver = webdriver.Chrome(options=option)
 try:
     driver.get('http://terabox.ru/index.php')
@@ -17,11 +16,19 @@ try:
     time.sleep(1)
     driver.find_element(By.XPATH, '/html/body/table/tbody/tr[11]/td/form/input[3]').click()
     time.sleep(5)
+    if driver.find_element(By.XPATH, '//*[@id="block"]/table/tbody/tr/td/table/tbody/tr[2]/td[13]') != '00:00:00':
+        driver.find_element(By.XPATH, '//*[@id="block"]/table/tbody/tr/td/table/tbody/tr[2]/td[14]').click()
+    time.sleep(5)
+    # response = requests.get('http://terabox.ru/index.php')
+    # src = response.text
+    # print(src)
 
+#
+# //*[@id="block"]/table/tbody/tr/td/table/tbody/tr[2]/td[12]
+# /table/tbody/tr/td/table/tbody/tr[2]/td[2]
+# table > tbody > tr > td > table > tbody > tr:nth-child(2) > td:nth-child(14)
 except Exception as ex:
     print()
 finally:
     driver.close()
     driver.quit()
-
-
