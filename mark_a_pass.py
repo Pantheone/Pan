b33d7f8f5c506f17424ba4fd9b2d5aca889399a1
Pantulip <terpelov_p@weberlibs.ru>
2024-01-11 16:28:34 +0300

    work check

--- mark_a_pass.py
+++ mark_a_pass.py
@@ -19,39 +19,16 @@ try:
     driver.find_element(By.NAME, "check").click()
     time.sleep(1)
     driver.find_element(By.XPATH, '/html/body/table/tbody/tr[11]/td/form/input[3]').click()
-    time.sleep(30)
-
-    num = 2
-    # a = f'/html/body/table/tbody/tr[{num}]/td[13]'
-    # b = f'/html/body/table/tbody/tr[{num}]/td[14]'
-
-    for num in range(2, 21):
-        if (f'/html/body/table/tbody/tr[{num}]/td[13]' != '00:00:00' and
-                f'/html/body/table/tbody/tr[{num}]/td[14]' == '00:00:00'):
-            driver.find_element(By.XPATH, f'/html/body/table/tbody/tr[{num}]/td[14]').click()
-        else:
-            continue
-        num += 1
-
-    # while num < 20:
-    #     if f'/html/body/table/tbody/tr[{num}]/td[13]' != '00:00:00':
-    #         driver.find_element(By.XPATH, f'/html/body/table/tbody/tr[{num}]/td[14]').click()
-    #     else:
-    #         continue
-    #     num += 1
-    # if driver.find_element(By.XPATH, '//*[@id="block"]/table/tbody/tr/td/table/tbody/tr[2]/td[13]') != '00:00:00':
-    #     driver.find_element(By.XPATH, '//*[@id="block"]/table/tbody/tr/td/table/tbody/tr[2]/td[14]').click()
-    # time.sleep(5)
-    # response = requests.get('http://terabox.ru/index.php')
-    # src = response.text
-    # print(src)
-
-#
-# //*[@id="block"]/table/tbody/tr/td/table/tbody/tr[2]/td[12]
-# /table/tbody/tr/td/table/tbody/tr[2]/td[2]
-# table > tbody > tr > td > table > tbody > tr:nth-child(2) > td:nth-child(14)
-# except Exception as ex:
-#     print()
+    time.sleep(10)
+    while 1 < 2:
+        num = 2
+        for num in range(2, 21):
+            if (f'/html/body/table/tbody/tr[{num}]/td[13]' != '00:00:00' and
+                    f'/html/body/table/tbody/tr[{num}]/td[14]' == '00:00:00'):
+                driver.find_element(By.XPATH, f'/html/body/table/tbody/tr[{num}]/td[14]').click()
+            else:
+                continue
+            num += 1
 finally:
     driver.close()
     driver.quit()
