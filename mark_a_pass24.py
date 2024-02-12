dc87e1900ac482c279b65c09e042f79783ccc8c3
Pantulip <terpelov_p@weberlibs.ru>
2024-02-12 11:04:06 +0300

    Исключение

--- mark_a_pass24.py
+++ mark_a_pass24.py
@@ -11,7 +11,6 @@ option.add_argument("--no-sandbox")
 option.add_argument("--headless")
 option.add_argument("--disable-gpu")
 
-
 driver.get('http://terabox.ru/index.php')
 time.sleep(2)
 pass_input = driver.find_element(By.NAME, 'pass')
@@ -24,24 +23,21 @@ time.sleep(3)
 while 1 < 2:
     try:
         num = 2
-        for num in range(2,50):
+        for num in range(2, 50):
             if (driver.find_element(By.XPATH,
-                                                f'html/body/table/tbody/tr[2]/td/div/table/tbody/tr/td/table/tbody/tr[{num}]/td[13]').text != '00:00:00'
+                                    f'html/body/table/tbody/tr[2]/td/div/table/tbody/tr/td/table/tbody/tr[{num}]/td[13]').text == '00:00:00'
                     and driver.find_element(By.XPATH,
-                                    f'html/body/table/tbody/tr[2]/td/div/table/tbody/tr/td/table/tbody/tr[{num}]/td[14]').text == '00:00:00'):
+                                            f'html/body/table/tbody/tr[2]/td/div/table/tbody/tr/td/table/tbody/tr[{num}]/td[14]').text != '00:00:00'):
 
-                driver.find_element(By.XPATH,f'/html/body/table/tbody/tr[2]/td/div/table/tbody/tr/td/table/tbody/tr[{num}]/td[14]').click()
+                driver.find_element(By.XPATH,
+                                    f'/html/body/table/tbody/tr[2]/td/div/table/tbody/tr/td/table/tbody/tr[{num}]/td[14]').click()
+                time.sleep(3)
             else:
                 continue
+                time.sleep(3)
             num += 1
-    except NoSuchElementException:
+    except NoSuchElementException:  # Исключение
         pass
 
-# finally:
 #     driver.close()
 #     driver.quit()
-
-
-
-
-
